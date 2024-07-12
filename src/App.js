2644b9d753b5ac974e1c1fa59de2f17fb5ca44a8
sdhan <sdhan@suresofttech.com>
2024-07-12 13:40:41 +0900

    [LIFECODING] 1 ~ 4. props
    [LIFECODING] 1 ~ 4 props
    Component + props 예제 풀이

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
 
+const list = [{
+  id: 1, 
+  title: "HTML" 
+},
+{
+  id: 2, 
+  title: "CSS" 
+},
+{
+  id: 3, 
+  title: "JAVASCRIPT" 
+}
+]
+
+function Header(props) {
+  return <header> 
+      <h1><a href="/"> {props.title} </a></h1>
+      </header>
+}
+
+function Article(props) {
+  return <article>
+      <h2>{props.test}</h2>
+          {props.bodies}
+      </article>
+}
+
+/**
+ * 
+ * @param {*} props 
+ * @returns 
+ */
+let Nav = (props) => {
+  const combin_list = [];
+  for(let i = 0; i < props.list.length; i++) {
+    combin_list.push(<li key={props.list[i].id}>
+      <a href={'/read/'+ props.list[i].id}>{props.list[i].title}</a>
+      </li>)
+  }  
+  return <nav>
+        <ol>
+          {combin_list}
+          {/* <li><a href="/read/1"> HTML </a></li>
+          <li><a href="/read/2"> CSS </a></li>
+          <li><a href="/read/3"> JS </a></li> */}
+        </ol>
+      </nav>
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header title="REACT"></Header>
+      <Nav list = {list}></Nav>
+      <Article test="WELCOME" bodies = "HELLO WROLD"></Article>
     </div>
   );
 }
